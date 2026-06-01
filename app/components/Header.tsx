31ee4899bb0911b657040735f1f8e2b7697590a1
Joshua Pozzolungo <z5599337@ad.unsw.edu.au>
2026-06-01 22:16:38 +1000

    started the header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,21 @@
 export default function Header() {
   return (
-    <header className="w-full h-50 border-b-2 border-b-black/10">
-      Header Page
+    <header className="w-full h-30 border-b-2 border-b-black/10">
+      <div className="flex w-full h-30 px-20 justify-between items-center">
+        
+        <div className="flex">
+          Logo
+        </div>
+
+        <div className="flex">
+          Menu Items
+        </div>
+        <div className="flex gap-x-5">
+          <button className="px-4 py-2 rounded-md hover:cursor-pointer hover:bg-bg-primary-hover">Log In</button>
+          <button className="px-4 py-2 rounded-md bg-bg-primary text-white hover:cursor-pointer hover:bg-bg-primary/80">Sign Up</button>
+        </div>
+
+      </div>
     </header>
   )
 }
