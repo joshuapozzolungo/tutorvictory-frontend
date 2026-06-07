eb41aa669174406514783f0555727c5d5b11cbe8
Joshua Pozzolungo <z5599337@ad.unsw.edu.au>
2026-06-08 08:15:38 +1000

    finished sign in page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import Link from "next/link"
 import "./login.css";
 
@@ -12,58 +13,75 @@ export default function LoginPage() {
           </Link>
         </div>
 
-        <div className="bg-white rounded-md  px-10 py-5 space-y-5">
+        <div className="bg-white rounded-md px-10 py-10 flex flex-col gap-y-5">
 
           <h1 className="text-xl font-bold text-center">Sign In</h1>
           
-          <div className="relative">
-            <input 
-              id="email"
-              inputMode="email" 
-              placeholder=" "
-                   type="text" 
-                   required 
-                   autoComplete="email" 
-                   autoCapitalize="none" 
-                   spellCheck="false"
-                   className="peer w-full px-2 py-3 focus:border-[#6D28FF] focus:ring-1 focus:ring-call-to-action border border-black/20 rounded-md"/>
-            <label htmlFor="email" className="absolute bg-white text-sm left-1/20 top-1/2 -translate-y-1/2 transition-all duration-200 
-                peer-focus:top-0 peer-focus:text-xs peer-focus:left-2 peer-[:not(:placeholder-shown)]:top-0 peer-[:not(:placeholder-shown)]:text-xs
-                peer-[:not(:placeholder-shown)]:left-2">
-              Email*
-            </label>
-          </div>
+          <div className="flex flex-col gap-y-3">
 
-          <div className="relative">
-            <input 
-              id="password"
-              inputMode="text" 
-              placeholder=" "
-                   type="text" 
-                   required 
-                   autoComplete="current-password" 
-                   autoCapitalize="none" 
-                   spellCheck="false"
-                   className="peer w-full px-2 py-3 focus:ring-1 focus:ring-call-to-action border border-black/20 rounded-md"/>
-            <label htmlFor="password" className="absolute bg-white text-sm left-1/20 top-1/2 -translate-y-1/2 transition-all duration-200 
-                peer-focus:top-0 peer-focus:text-xs peer-focus:left-2 peer-[:not(:placeholder-shown)]:top-0 peer-[:not(:placeholder-shown)]:text-xs
-                peer-[:not(:placeholder-shown)]:left-2 focus:border-call-to-action focus:outline-none ">
-              Password*
-            </label>
+            <div className="relative">
+              <input 
+                id="email"
+                inputMode="email" 
+                placeholder=" "
+                     type="text" 
+                     required 
+                     autoComplete="email" 
+                     autoCapitalize="none" 
+                     spellCheck="false"
+                     className="peer w-full px-2 py-3 focus:border-[#6D28FF] focus:ring-1 focus:ring-call-to-action border border-black/20 rounded-md"/>
+              <label htmlFor="email" className="absolute bg-white text-sm left-1/20 top-1/2 -translate-y-1/2 transition-all duration-200 
+                  peer-focus:top-0 peer-focus:text-xs peer-focus:left-2 peer-[:not(:placeholder-shown)]:top-0 peer-[:not(:placeholder-shown)]:text-xs
+                  peer-[:not(:placeholder-shown)]:left-2">
+                Email*
+              </label>
+            </div>
+
+            <div className="relative">
+              <input 
+                id="password"
+                inputMode="text" 
+                placeholder=" "
+                     type="password" 
+                     required 
+                     autoComplete="current-password" 
+                     autoCapitalize="none" 
+                     spellCheck="false"
+                     className="peer w-full px-2 py-3 focus:ring-1 focus:ring-call-to-action border border-black/20 rounded-md"/>
+              <label htmlFor="password" className="absolute bg-white text-sm left-1/20 top-1/2 -translate-y-1/2 transition-all duration-200 
+                  peer-focus:top-0 peer-focus:text-xs peer-focus:left-2 peer-[:not(:placeholder-shown)]:top-0 peer-[:not(:placeholder-shown)]:text-xs
+                  peer-[:not(:placeholder-shown)]:left-2 focus:border-call-to-action focus:outline-none ">
+                Password*
+              </label>
+            </div>
           </div>
           
           <a className="text-call-to-action text-medium text-sm hover:cursor-pointer hover:underline self-left">
             Forgot your password?
           </a>
 
-          <button className="block bg-call-to-action p-3 rounded-md text-white text-sm text-medium hover:cursor-pointer w-full">
+          <button className="block bg-call-to-action p-3 rounded-md text-white text-sm font-medium hover:cursor-pointer hover:bg-call-to-action/80 w-full">
             Log In
           </button>
 
           <div className="or">
             <span>OR</span>
           </div>
+          
+          <button className="block p-3 flex justify-center items-center gap-x-2 
+            rounded-md text-white text-sm font-medium hover:cursor-pointer w-full 
+            border border-black/20 rounded-md hover:bg-black/2">
+            <span>
+              <Image src="/google.svg" width={20} height={20} alt="google"/>
+            </span>
+            <span className="text-black font-medium">Sign in with Google</span>
+          </button>
+
         </div>
+
+        <p className="text-center text-sm fold-medium pt-5 ">Don't have an account yet? 
+          <a className="pl-1 text-call-to-action font-bold hover:underline hover:cursor-pointer">Register</a>
+        </p>
       </div>
     </div>
   )
