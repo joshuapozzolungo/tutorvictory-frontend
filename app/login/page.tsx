114a141ec2d4178a984146247b0a8459b2f37a37
Joshua Pozzolungo <z5599337@ad.unsw.edu.au>
2026-06-07 21:48:56 +1000

    working on sign up page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import "./login.css";
 
 export default function LoginPage() {
   return (
@@ -11,9 +12,9 @@ export default function LoginPage() {
           </Link>
         </div>
 
-        <div className="bg-white rounded-md text-center px-10 py-5 space-y-5">
+        <div className="bg-white rounded-md  px-10 py-5 space-y-5">
 
-          <h1 className="text-xl font-bold">Sign In</h1>
+          <h1 className="text-xl font-bold text-center">Sign In</h1>
           
           <div className="relative">
             <input 
@@ -26,11 +27,41 @@ export default function LoginPage() {
                    autoCapitalize="none" 
                    spellCheck="false"
                    className="peer w-full px-2 py-3 focus:border-[#6D28FF] focus:ring-1 focus:ring-call-to-action border border-black/20 rounded-md"/>
-            <label htmlFor="email" className="absolute bg-white text-md left-1/20 top-1/2 -translate-y-1/2 transition-all duration-200 
+            <label htmlFor="email" className="absolute bg-white text-sm left-1/20 top-1/2 -translate-y-1/2 transition-all duration-200 
                 peer-focus:top-0 peer-focus:text-xs peer-focus:left-2 peer-[:not(:placeholder-shown)]:top-0 peer-[:not(:placeholder-shown)]:text-xs
                 peer-[:not(:placeholder-shown)]:left-2">
               Email*
             </label>
+          </div>
+
+          <div className="relative">
+            <input 
+              id="password"
+              inputMode="text" 
+              placeholder=" "
+                   type="text" 
+                   required 
+                   autoComplete="current-password" 
+                   autoCapitalize="none" 
+                   spellCheck="false"
+                   className="peer w-full px-2 py-3 focus:ring-1 focus:ring-call-to-action border border-black/20 rounded-md"/>
+            <label htmlFor="password" className="absolute bg-white text-sm left-1/20 top-1/2 -translate-y-1/2 transition-all duration-200 
+                peer-focus:top-0 peer-focus:text-xs peer-focus:left-2 peer-[:not(:placeholder-shown)]:top-0 peer-[:not(:placeholder-shown)]:text-xs
+                peer-[:not(:placeholder-shown)]:left-2 focus:border-call-to-action focus:outline-none ">
+              Password*
+            </label>
+          </div>
+          
+          <a className="text-call-to-action text-medium text-sm hover:cursor-pointer hover:underline self-left">
+            Forgot your password?
+          </a>
+
+          <button className="block bg-call-to-action p-3 rounded-md text-white text-sm text-medium hover:cursor-pointer w-full">
+            Log In
+          </button>
+
+          <div className="or">
+            <span>OR</span>
           </div>
         </div>
       </div>
