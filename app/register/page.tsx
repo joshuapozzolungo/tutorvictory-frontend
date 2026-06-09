d4990be05e25436d6781a5b4fd132fe9673a18d5
Joshua Pozzolungo <z5599337@ad.unsw.edu.au>
2026-06-09 22:44:50 +1000

    started rego page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,23 +1,44 @@
+import Link from "next/link";
 import Image from "next/image"
-import Link from "next/link"
-import "./login.css";
 
-export default function LoginPage() {
+const passwordRequirements = [{label: "at least eight characters", test: (p:string) => p.length >= 8},
+                              {label: "one uppercase letter", test: (p:string) => /[A-Z]/.test(p)},
+                              {label: "one lowercase letter", test: (p:string) => /[a-z]/.test(p)},
+                              {label: "one number", test: (p:string) => /[0-9]/.test(p)},
+                              {label: "one special character", test: (p:string) => /\W/.test(p)},
+]
+
+export default function RegisterPage() {
+
   return (
-    <div className="flex min-h-screen w-full items-center justify-center bg-[#EFEAFF]">
-      <div className="w-[40%] max-w-md">
-        
-        <div className="mb-6 text-center text-2xl font-semibold">
-          <Link href="/">
-            <span>Tutor Victory</span>
-          </Link>
-        </div>
+    <div className="w-full min-h-screen grid grid-cols-2">
+      <div className="bg-bg-primary-hover">First Col</div>
+      
+      <div className="flex flex-col items-center justify-center bg-bg-base">
 
-        <div className="bg-white rounded-md px-10 py-10 flex flex-col gap-y-5">
-
-          <h1 className="text-xl font-bold text-center">Sign In</h1>
+        <div className="flex flex-col gap-y-3 w-[65%] h-[50%] rounded-md p-5">
           
-          <div className="flex flex-col gap-y-3">
+          <h1 className="text-xl font-bold text-center">Create your account</h1>
+          
+          <div className="flex flex-col gap-y-4">
+            
+            <div className="relative">
+              <input 
+                id="name"
+                inputMode="text" 
+                placeholder=" "
+                     type="text" 
+                     required 
+                     autoComplete="name" 
+                     autoCapitalize="none" 
+                     spellCheck="false"
+                     className="peer w-full px-2 py-3 focus:border-[#6D28FF] focus:ring-1 focus:ring-call-to-action border border-black/20 rounded-md"/>
+              <label htmlFor="email" className="absolute bg-white text-sm left-1/20 top-1/2 -translate-y-1/2 transition-all duration-200 
+                  peer-focus:top-0 peer-focus:text-xs peer-focus:left-2 peer-[:not(:placeholder-shown)]:top-0 peer-[:not(:placeholder-shown)]:text-xs
+                  peer-[:not(:placeholder-shown)]:left-2">
+                Name*
+              </label>
+            </div>
 
             <div className="relative">
               <input 
@@ -56,31 +77,23 @@ export default function LoginPage() {
             </div>
           </div>
           
-          <a className="text-call-to-action text-medium text-sm hover:cursor-pointer hover:underline self-left">
-            Forgot your password?
-          </a>
-
           <button className="block bg-call-to-action p-3 rounded-md text-white text-sm font-medium hover:cursor-pointer hover:bg-call-to-action/80 w-full">
-            Log In
+           Sign Up 
           </button>
 
-          <div className="or">
-            <span>OR</span>
-          </div>
-          
           <button className="block p-3 flex justify-center items-center gap-x-2 
             rounded-md text-white text-sm font-medium hover:cursor-pointer w-full 
             border border-black/20 rounded-md hover:bg-black/2">
             <span>
               <Image src="/google.svg" width={20} height={20} alt="google"/>
             </span>
-            <span className="text-black font-medium">Sign in with Google</span>
+            <span className="text-black font-medium">Sign up with Google</span>
           </button>
 
         </div>
 
-        <p className="text-center text-sm fold-medium pt-5 ">Don't have an account yet? 
-          <Link href="/register" className="pl-1 text-call-to-action font-bold hover:underline hover:cursor-pointer">Register</Link>
+        <p className="font-medium text-sm">Already have an account?
+          <Link href="/login" className="font-bold text-sm text-call-to-action pl-1 hover:underline">Sign In</Link>
         </p>
       </div>
     </div>
