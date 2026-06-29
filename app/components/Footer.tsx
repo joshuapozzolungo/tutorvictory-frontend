9eb9b91c87bab1584a3369147512bbde83ea5482
Joshua Pozzolungo <z5599337@ad.unsw.edu.au>
2026-06-29 22:38:35 +1000

    almost finished the footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,40 +1,56 @@
 import Link from "next/link";
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import XIcon from '@mui/icons-material/X';
 
 export default function Footer() {
 
-  const footer_headings = ["Product", "Resources", "Company", "Join our Community"]
+  const footer_headings = ["Content", "Resources", "Company", "Careers", "Community"]
   
   return (
-    <div className="w-full min-h-120 bg-primary flex flex-col justify-between px-20 pt-20">
-     
-      <div className="flex h-[90%]">
-        <div className="w-full flex gap-x-30">
+    <footer className="w-full min-h-120 bg-primary flex flex-col justify-between">
+
+      { /* Top Half */}
+      <div className="flex w-full p-15">
+        <div className="flex flex-col w-[60%] gap-y-3">
+          <Link href="/" className="text-2xl whitespace-nowrap font-bold text-heading">Tutor Victory</Link>
+          <p className="text-paragraph text-sm max-w-[70%]">Stay informed on feature releases, product roadmap, support, and cloud offerings!</p>
+
+          <div className="flex gap-x-5 text-paragraph">
+            <LinkedInIcon/>
+            <YouTubeIcon/>
+            <InstagramIcon/>
+            <XIcon/> 
+          </div>
+        </div>
+
+        <div className="grid grid-cols-3 w-full">
           {footer_headings.map((heading) => (
-            <div className="text-heading font-bold">
-              {heading}
+            <div className="items-center justify-center text-heading font-bold text-sm">
+              <div>
+                {heading}
+              </div>
             </div>
           ))}
+        </div>
 
-          <div className="flex flex-col gap-y-5">
-            <Link href="/" className="text-2xl whitespace-nowrap font-bold text-white">Tutor Victory</Link>
-            <p className="text-paragraph text-sm">Stay informed on feature releases, product roadmap, support, and cloud offerings!</p>
-            <form className="relative bg-[#1C1C1A] rounded-md text-sm">
-              <input type="email" placeholder="Enter your email" className="p-3 w-full pr-10 text-paragraph outline-none"/>
-              <button className="px-4 py-2 rounded-md bg-call-to-action hover:cursor-pointer hover:bg-bg-primary/80 absolute right-2 top-1/2 -translate-y-1/2">Sign Up</button>
-            </form>
+
+      </div>
+
+      { /* Bottom Half */}
+      <div className="w-full">
+        <div className="flex justify-between w-full h-[10%] text-sm bg-gray-50 p-15">
+          <div className="text-paragraph"> © 2026 Tutor Victory, All Rights Reserved. </div>
+          <div className="text-paragraph flex gap-x-5">
+            <Link href="/cancellation-policy" className="hover:underline hover:cursor-pointer">Cancellation Policy</Link>
+            <Link href="/terms-of-service" className="hover:underline hover:cursor-pointer">Terms of Service</Link>
+            <Link href="/payment-policy" className="hover:underline hover:cursor-pointer">Payment Policy</Link>
+            <Link href="/privacy-policy" className="hover:underline hover:cursor-pointer">Privacy Policy</Link>
           </div>
         </div>
       </div>
 
-      <div className="flex justify-between w-full h-[10%] p-10 text-sm bg-gray-50">
-        <div className="text-paragraph"> © 2026 Tutor Victory, All Rights Reserved. </div>
-        <div className="text-paragraph flex gap-x-5">
-          <Link href="/cancellation-policy" className="hover:underline hover:cursor-pointer">Cancellation Policy</Link>
-          <Link href="/terms-of-service" className="hover:underline hover:cursor-pointer">Terms of Service</Link>
-          <Link href="/payment-policy" className="hover:underline hover:cursor-pointer">Payment Policy</Link>
-          <Link href="/privacy-policy" className="hover:underline hover:cursor-pointer">Privacy Policy</Link>
-        </div>
-      </div>
-    </div>
+    </footer>
   )
 }
