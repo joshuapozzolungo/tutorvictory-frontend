f6191a5c32cb1ad2b7072b5e04bd020022797dcd
Joshua Pozzolungo <z5599337@ad.unsw.edu.au>
2026-06-30 11:29:02 +1000

    made footer responsive

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -8,8 +8,8 @@ export default function Footer() {
 
   const footer_headings = [
       {main: "Content", submenus: [{heading: "Questions", url: "questions"}, {heading: "Courses", url: "courses"}, {heading: "Playlists", url: "playlists"}, {heading: "Learning", url: "learning"}]},
-      {main: "Resources", submenus: [{heading: "Blog", url: "blog"}, {heading: "Frequently Asked Questions", url: "faqs"}, {heading: "Changelog", url: "changelog"}, {heading: "Book a Consultation", url: "book-a-consultation"}]},
       {main: "Company", submenus: [{heading: "About", url: "about"}, {heading: "Jobs", url: "jobs"}, {heading: "Contact", url: "contact"}, {heading: "Contribute", url: "contribute"}]},
+      {main: "Resources", submenus: [{heading: "Blog", url: "blog"}, {heading: "Frequently Asked Questions", url: "faqs"}, {heading: "Changelog", url: "changelog"}, {heading: "Book a Consultation", url: "book-a-consultation"}]},
       {main: "Careers", submenus: [{heading: "Book a Mentor", url: "book-a-mentor"}, {heading: "Become a Mentor", url: "become-a-mentor"}, {heading: "Recruitment", url: "recruitment"}, {heading: "Guides", url: "guides"}]},
       {main: "Community", submenus: [{heading: "Submit Questions", url: "submit-questions"}, {heading: "Affiliate Program", url: "affiliate-program"}, {heading: "Forums", url: "forums"}, {heading: "Feedback", url: "feedback"}]}
   ]
@@ -18,12 +18,13 @@ export default function Footer() {
     <footer className="w-full min-h-120 flex flex-col justify-between border-t border-t-paragraph/20 bg-primary">
 
       { /* Top Half */}
-      <div className="flex w-full px-15 pt-15">
-        <div className="flex flex-col w-[80%] gap-y-5">
-          <Link href="/" className="text-2xl whitespace-nowrap font-bold text-heading">Tutor Victory</Link>
-          <p className="text-paragraph text-sm max-w-[60%]">Stay informed on feature releases, product roadmap, support, and cloud offerings!</p>
+      <div className="flex flex-col sm:flex-row items-start gap-y-15 w-full px-10 sm:px-15 pt-15">
 
-          <div className="flex gap-x-5 text-paragraph">
+        <div className="flex flex-col w-full sm:w-[80%] mx-auto gap-y-5 px-5">
+          <Link href="/" className="text-2xl whitespace-nowrap font-bold text-heading">Tutor Victory</Link>
+          <p className="text-paragraph text-sm max-w-[80%] sm:max-w-[60%]">Stay informed on feature releases, product roadmap, support, and cloud offerings!</p>
+
+          <div className="flex gap-x-5 text-paragraph items-center sm:items-start">
             <LinkedInIcon className="hover:text-heading hover:cursor-pointer"/>
             <YouTubeIcon className="hover:text-heading hover:cursor-pointer"/>
             <InstagramIcon className="hover:text-heading hover:cursor-pointer"/>
@@ -31,9 +32,9 @@ export default function Footer() {
           </div>
         </div>
 
-        <div className="grid grid-cols-3 w-full">
+        <div className="grid grid-cols-3 w-full md:text-left">
           {footer_headings.map((heading) => (
-            <div key={heading} className="items-center justify-center text-heading font-bold text-sm">
+            <div key={heading} className="items-center justify-center text-heading font-bold text-sm pl-5">
               <h4 className="pb-3">{heading.main}</h4>
               <div className="flex flex-col gap-y-3 pb-10">
                 {heading.submenus.map(submenu => (
@@ -48,13 +49,16 @@ export default function Footer() {
 
       { /* Bottom Half */}
       <div className="w-full">
-        <div className="flex justify-between w-full h-[10%] text-sm bg-gray-50 p-15">
+        <div className="flex flex-col sm:flex-row justify-between w-full h-[10%] text-xs sm:text-sm bg-gray-50 p-5 sm:p-15 gap-y-2">
           <div className="text-paragraph"> © 2026 Tutor Victory, All Rights Reserved. </div>
-          <div className="text-paragraph flex gap-x-5">
+          <div className="text-paragraph flex gap-x-2 sm:gap-x-5">
             <Link href="/cancellation-policy" className="hover:underline hover:cursor-pointer">Cancellation Policy</Link>
-            <Link href="/terms-of-service" className="hover:underline hover:cursor-pointer">Terms of Service</Link>
-            <Link href="/payment-policy" className="hover:underline hover:cursor-pointer">Payment Policy</Link>
-            <Link href="/privacy-policy" className="hover:underline hover:cursor-pointer">Privacy Policy</Link>
+            <span className="block px-1 sm:hidden">|</span>
+            <Link href="/terms-of-service" className="hover:underline hover:cursor-pointer ">Terms of Service</Link>
+            <span className="block px-1 sm:hidden">|</span>
+            <Link href="/payment-policy" className="hover:underline hover:cursor-pointer ">Payment Policy</Link>
+            <span className="block px-1 sm:hidden">|</span>
+            <Link href="/privacy-policy" className="hover:underline hover:cursor-pointer ">Privacy Policy</Link>
           </div>
         </div>
       </div>
